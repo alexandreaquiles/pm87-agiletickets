e555647f41df8ef631d6f48a0c74ab87443a307c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-04-10 19:26:43 -0300

    Refatorando cenários e steps de estacionamento

--- src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/estacionamento/EstacionamentoSteps.java
+++ src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/estacionamento/EstacionamentoSteps.java
@@ -1,51 +1,34 @@
 package br.com.caelum.agiletickets.acceptance.estacionamento;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
+import br.com.caelum.agiletickets.acceptance.SharedDriver;
 import br.com.caelum.agiletickets.acceptance.page.EstabelecimentosPage;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class EstacionamentoSteps {
 	
-	public static String BASE_URL = "http://localhost:8080";
-
-	private WebDriver browser;
 	private EstabelecimentosPage estabelecimentos;
 	
-	@Before
-	public void inicia(){
-		browser = new FirefoxDriver();
-		estabelecimentos = new EstabelecimentosPage(browser);
-	}
-	
-	@After
-	public void finaliza(){
-		browser.close();
-	}
-	
-	@Dado("que o usuário abre a lista de estabelecimentos")
-	public void abreListagem(){
-		estabelecimentos.abreListagem();
+	public EstacionamentoSteps(EstabelecimentosPage estabelecimentos) {
+		this.estabelecimentos = estabelecimentos;
 	}
 	
 	@Quando("o usuário marca que (há|não há) estacionamento")
 	public void marcaEstacionamento(String temEstacionamento){
-		estabelecimentos.adicioneEstabelecimentoComEstacionamento("há".equals(temEstacionamento)?true:false);
+		estabelecimentos.adicioneEstabelecimentoComEstacionamento(temOuNao(temEstacionamento));
 	}
 	
-	@Quando("clica em \"Adicionar\"")
-	public void clicaAdicionar(){
-		estabelecimentos.adicionar();
-	}
 
 	@Então("a última linha da lista mostra que (há|não há) estacionamento")
 	public void ultimaLinhaDeveTerEstacionamento(String temEstacionamento) {
-		estabelecimentos.ultimaLinhaDeveTerEstacionamento("há".equals(temEstacionamento)?true:false);
+		estabelecimentos.ultimaLinhaDeveTerEstacionamento(temOuNao(temEstacionamento));
 	}
 
+
+	public boolean temOuNao(String temEstacionamento) {
+		return "há".equals(temEstacionamento)?true:false;
+	}
+	
 }
--- src/acceptance-test/resources/estacionamento.feature
+++ src/acceptance-test/resources/estacionamento.feature
@@ -5,14 +5,14 @@ Funcionalidade: Cadastro de Estacionamento
   Como dono também de uma frota de táxis
   Quero poder cadastrar se um estabelecimento tem estacinamento 
 
-  Cenário:
+  Cenário: há estacionamento
     Dado que o usuário abre a lista de estabelecimentos
     Quando o usuário marca que há estacionamento
     E clica em "Adicionar"
     Então a última linha da lista mostra que há estacionamento
     
-  Cenário:
+  Cenário: não há estacionamento
     Dado que o usuário abre a lista de estabelecimentos
     Quando o usuário marca que não há estacionamento
     E clica em "Adicionar"
-    Então a última linha da lista mostra que não há estacionamento    
+    Então a última linha da lista mostra que não há estacionamento
